25a4b12a200ac0b2981a5ed3476c4dc97acbea2c
gioe <gioematt@gmail.com>
2016-07-22 08:34:38 -0400

    define django_root

--- OneCastServer/settings/base.py
+++ OneCastServer/settings/base.py
@@ -17,6 +17,7 @@ import djcelery
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
